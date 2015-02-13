0dfffd7f8f3e8fe31fd12f662fdc96e4618dafab
Dony Thomas <dony.thomas7@gmail.com>
2015-02-13 00:14:01 -0500

    1st commit

---
+++ ExtJSDemo/src/main/webapp/resources/01-basics/app.js
@@ -0,0 +1,14 @@
+Ext.onReady(function() {
+	// alert("We're ready to go!");
+	// Ext.Msg.alert("Alert","We're ready to go!");
+	Ext.Msg.confirm("Confirm", "Do you like Ext JS?", response);
+
+});
+
+function response(btn) {
+	if (btn === "yes") {
+		Ext.Msg.alert("Great!", "This is great!");
+	} else {
+		Ext.Msg.alert("Really?", "That's too bad.");
+	}
+}
