64acacf29fbd28040def3d1cdbd5a45e0b6b6e28
Dony Thomas <dony.thomas7@gmail.com>
2015-02-22 18:46:10 -0500

    9th commit

--- ExtJSDemo/src/main/webapp/app.js
+++ ExtJSDemo/src/main/webapp/app.js
@@ -1,36 +1,60 @@
 Ext.onReady(function() {
-	
-	Ext.create('Ext.form.Panel', {
-	    title: 'FieldContainer Example',
-	    width: 550,
-	    bodyPadding: 10,
 
-	    items: [{
-	        xtype: 'fieldcontainer',
-	        fieldLabel: 'Last Three Jobs',
-	        labelWidth: 100,
+	var myTpl = Ext.create('Ext.Template', [
+			'<div style="background-color: {color};">',
+			'<b> Welcome: </b> {name}<br />', '</div>' ]);
 
-	        // The body area will contain three text fields, arranged
-	        // horizontally, separated by draggable splitters.
-	        layout: 'vbox',
-	        items: [{
-	            xtype: 'textfield',
-	            flex: 1
-	        }, {
-	            xtype: 'splitter'
-	        }, {
-	            xtype: 'textfield',
-	            flex: 1
-	        }, {
-	            xtype: 'splitter'
-	        }, {
-	            xtype: 'textfield',
-	            flex: 1
-	        }]
-	    }],
-	    renderTo: Ext.getBody()
-	});
-	
-	//filterPanel.center();
-	
+	myTpl.compile();
+
+	var win = Ext
+			.create(
+					'Ext.container.Viewport',
+					{
+						layout : {
+							type : 'border',
+							//padding : 2
+						},
+						items : [
+								{
+									region : 'north',
+									margins : '0 0 0 0',
+									//title : 'Consultant Home',
+									height : 70,
+/*									tpl : myTpl,
+									data : {
+										name : 'Carl',
+										color : 'white'
+									}*/
+									html : "<div id='header'><table><tr><td class='td-left-data'>Welcome, Don</td><td></td><td class='td-right-data'>Logout</td></tr></table></div>"
+								},
+								{
+									region : 'west',
+									xtype : 'panel',
+									title : 'Navigation',
+									width : 200,
+									split : true,
+									collapsible : true,
+									floatable : false
+								}, {
+									region : 'center',
+									xtype : 'tabpanel',
+									items : [
+									/*
+									 * { rtl : false, title : 'Bogus Tab', html : '<p>Window
+									 * configured with:</p><pre style="margin-left:20px"><code>header:
+									 * {\n titlePosition: 2,\n titleAlign:
+									 * "center"\n},\nmaximizable: true,\ntools:
+									 * [{type: "pin"}],\nclosable: true</code></pre>' }, {
+									 * title : 'Another Tab', html : 'Hello
+									 * world 2' }, { title : 'Closable Tab',
+									 * html : 'Hello world 3', closable : true }
+									 */]
+								},{
+									region : 'south',
+									xtype : 'panel',
+									height : 20,
+									html: 'Southern Stuff here'
+								}  ]
+					}).show();
+
 });
--- ExtJSDemo/src/main/webapp/resources/01-basics/app.js
+++ ExtJSDemo/src/main/webapp/resources/01-basics/app.js
@@ -1,18 +1,51 @@
-Ext.define('MyApp.car.BaseCar', {
-        engine : 'I4',
-        turbo : false,
-        wheels : 'basic',
-        constructor : function(config) {
-            this.octaneRequired = 86;
-            this.shiftTo = function(gear) {
-                this.gear = gear;
-            };
-            this.shiftTo('park');
-        },
-        getEngine : function() {
-            return this.engine;
-        },
-        drive : function() {
-            console.log("Vrrrrooooooom - I'm driving!");
-        }
+Ext.onReady(function() {
+
+	var myTpl = Ext.create('Ext.Template', [
+			'<div style="background-color: {color};">',
+			'<b> Welcome, </b> {name}<br />', '</div>' ]);
+	
+	myTpl.compile();
+
+	var win = Ext.create('Ext.container.Viewport', {
+
+		// closable : false,
+		// maximized : true,
+		layout : {
+			type : 'border',
+			padding : 2
+		},
+		items : [ {
+			region : 'north',
+			margins : '0 0 5 0',
+			title : 'Consultant Home',
+			header : {
+				titleAlign : 'center'
+			},
+			height : 60,
+		    tpl: myTpl,
+		    data: {name: 'LeVeon', color : 'white'}
+		}, {
+			region : 'west',
+			xtype : 'panel',
+			title : 'Navigation',
+			width : 200,
+			split : true,
+			collapsible : false,
+			floatable : false
+		}, {
+			region : 'center',
+			xtype : 'tabpanel',
+			items : [
+			/*
+			 * { rtl : false, title : 'Bogus Tab', html : '<p>Window
+			 * configured with:</p><pre style="margin-left:20px"><code>header:
+			 * {\n titlePosition: 2,\n titleAlign: "center"\n},\nmaximizable:
+			 * true,\ntools: [{type: "pin"}],\nclosable: true</code></pre>' }, {
+			 * title : 'Another Tab', html : 'Hello world 2' }, { title :
+			 * 'Closable Tab', html : 'Hello world 3', closable : true }
+			 */]
+		} ]
+	}).show();
+	
+
 });
