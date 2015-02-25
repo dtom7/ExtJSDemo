57d4203f2fe233804b53ef4d716c41c90bc9c570
Dony Thomas <dony.thomas7@gmail.com>
2015-02-24 22:08:02 -0500

    13th commit

--- ExtJSDemo/src/main/webapp/app.js
+++ ExtJSDemo/src/main/webapp/app.js
@@ -3,23 +3,20 @@ Ext.require("My.model.Item");
 Ext.require("My.store.Invoices");
 Ext.onReady(function() {
 
-	var panel1 = {
-		html : 'I am Panel1',
-		id : 'panel1',
-		frame : false,
-		height : 100
-	};
-	var panel2 = {
-		html : '<b>I am Panel2</b>',
-		id : 'panel2',
-		frame : true
-	};
-	var myWin = Ext.create('Ext.window.Window', {
-		id : 'myWin',
-		height : 400,
-		width : 400,
-		items : [ panel1, panel2 ]
+	Ext.create('Ext.container.Viewport', {
+		layout : 'border',
+		items : [ {
+			height : 75,
+			region : 'north',
+			title : 'Does Santa live here?'
+		}, {
+			width : 150,
+			region : 'west',
+			title : 'The west region rules'
+		}, {
+			region : 'center',
+			title : 'No, this region rules!'
+		} ]
 	});
-	myWin.show();
 
 });
