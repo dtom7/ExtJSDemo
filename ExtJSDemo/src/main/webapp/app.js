dc79d65d3d89d140aa545da3502bba4e229284d3
Dony Thomas <dony.thomas7@gmail.com>
2015-02-25 16:37:07 -0500

    14th commit

--- ExtJSDemo/src/main/webapp/app.js
+++ ExtJSDemo/src/main/webapp/app.js
@@ -1,22 +1,43 @@
-Ext.require("My.model.Invoice");
-Ext.require("My.model.Item");
-Ext.require("My.store.Invoices");
 Ext.onReady(function() {
 
-	Ext.create('Ext.container.Viewport', {
-		layout : 'border',
-		items : [ {
-			height : 75,
-			region : 'north',
-			title : 'Does Santa live here?'
-		}, {
-			width : 150,
-			region : 'west',
-			title : 'The west region rules'
-		}, {
-			region : 'center',
-			title : 'No, this region rules!'
-		} ]
+	var win;
+	var newWindow = function(btn) {
+		if (win === undefined || win.isDestroyed === true) {
+			console.log('window created ..');
+			win = Ext.create('Ext.window.Window', {
+				//animateTarget : btn.el,
+				html : 'My first vanilla Window',
+				closeAction : 'hide',
+				id : 'myWin',
+				height : 200,
+				width : 300,
+				constrain : false
+			});
+			console.log('win is - ' + win.getId());
+		}
+		win.show();
+	};
+	new Ext.Button({
+		renderTo : Ext.getBody(),
+		text : 'Open my Window',
+		style : 'margin: 100px',
+		handler : newWindow
 	});
-
+/*	var comp;
+	console.log('comp is - ' + comp);
+	comp = Ext.create('Ext.Component', {
+		listeners : {
+			beforedestroy : function(comp) {
+				console.log('beforedestroy .. ');
+			},
+			destroy : function(comp) {
+				console.log('destroy .. ');
+			}
+		}
+	});
+	console.log('comp is - ' + comp);
+	console.log('comp id is - ' + comp.isDestroyed);
+	comp.destroy();
+	console.log('comp is - ' + comp);
+	console.log('comp id is - ' + comp.isDestroyed);*/
 });
