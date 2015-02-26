1e422eae329ae083c07885355f478ae9f80e9246
Dony Thomas <dony.thomas7@gmail.com>
2015-02-25 22:57:55 -0500

    15th commit

--- ExtJSDemo/src/main/webapp/app.js
+++ ExtJSDemo/src/main/webapp/app.js
@@ -1,43 +1,29 @@
 Ext.onReady(function() {
 
-	var win;
-	var newWindow = function(btn) {
-		if (win === undefined || win.isDestroyed === true) {
-			console.log('window created ..');
-			win = Ext.create('Ext.window.Window', {
-				//animateTarget : btn.el,
-				html : 'My first vanilla Window',
-				closeAction : 'hide',
-				id : 'myWin',
-				height : 200,
-				width : 300,
-				constrain : false
-			});
-			console.log('win is - ' + win.getId());
-		}
-		win.show();
-	};
-	new Ext.Button({
-		renderTo : Ext.getBody(),
-		text : 'Open my Window',
-		style : 'margin: 100px',
-		handler : newWindow
+	Ext.MessageBox.show({
+		title : 'Hold on there cowboy!',
+		msg : "We're doing something...",
+		progressText : 'Initializing...',
+		width : 300,
+		progress : true,
+		closable : false
 	});
-/*	var comp;
-	console.log('comp is - ' + comp);
-	comp = Ext.create('Ext.Component', {
-		listeners : {
-			beforedestroy : function(comp) {
-				console.log('beforedestroy .. ');
-			},
-			destroy : function(comp) {
-				console.log('destroy .. ');
+	var updateFn = function(num) {
+		setTimeout(function() {
+			if (num == 6) {
+				Ext.MessageBox.updateProgress(100, 'All Items saved !!');
+				Ext.Function.defer(Ext.MessageBox.hide, 1500, Ext.MessageBox);
+			} else {
+				var i = num / 6;
+				var pct = Math.round(100 * i);
+				Ext.MessageBox.updateProgress(i, pct + '% completed');
 			}
-		}
-	});
-	console.log('comp is - ' + comp);
-	console.log('comp id is - ' + comp.isDestroyed);
-	comp.destroy();
-	console.log('comp is - ' + comp);
-	console.log('comp id is - ' + comp.isDestroyed);*/
+		}, i * 500);
+	};
+	for (var i = 1; i < 7; i++) {
+		console.log("i " + i);
+		updateFn(i);
+		
+	}
+
 });
