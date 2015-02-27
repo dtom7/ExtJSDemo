6e1fc3c41a9a228526b23b6ae8fb231bc11ffe0b
Dony Thomas <dony.thomas7@gmail.com>
2015-02-27 16:25:20 -0500

    17th commit

--- ExtJSDemo/src/main/java/com/example/extjs/RESTController.java
+++ ExtJSDemo/src/main/java/com/example/extjs/RESTController.java
@@ -16,59 +16,78 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.extjs.domain.Greeting;
 import com.example.extjs.domain.Invoice;
 import com.example.extjs.domain.InvoiceJSONWrapper;
+import com.example.extjs.domain.LoginWrapper;
 import com.example.extjs.domain.Tab;
 import com.example.extjs.domain.TabWrapper;
 
 @RestController
 @RequestMapping("/REST")
 public class RESTController {
-	
-	
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
-    }
-    
-    @RequestMapping("/getInvoices")
-    public InvoiceJSONWrapper getInvoices() {
-    	DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-    	
-        List<Invoice> list = new ArrayList<>();
-        list.add(new Invoice(1000, "12345", "Victor", "123 Main St", df.format(new Date())));
-        list.add(new Invoice(1001, "12346", "Mark", "456 Main St", df.format(new Date())));
-        list.add(new Invoice(1002, "12347", "Carl", "789 Main St", df.format(new Date())));
-        list.add(new Invoice(1003, "12348", "Zen", "784 Main St", df.format(new Date())));
-        list.add(new Invoice(1004, "12349", "Chris", "258 Main St", df.format(new Date())));
-        list.add(new Invoice(1005, "12346", "Benny", "322 Main St", df.format(new Date())));
-        list.add(new Invoice(1006, "12351", "Sunny", "951 Main St", df.format(new Date())));
-        list.add(new Invoice(1007, "12352", "Kumar", "878 Main St", df.format(new Date())));
-        list.add(new Invoice(1008, "12351", "Nino", "357 Main St", df.format(new Date())));
-        list.add(new Invoice(1009, "12354", "Ivan", "985 Main St", df.format(new Date())));
-        
-        return new InvoiceJSONWrapper(true, list);
-    }
-    
-    @RequestMapping(value="/postInvoices",  method = RequestMethod.POST)
-    public boolean postInvoices(@RequestBody List<Invoice> list) {
-    	System.out.println("List size: " + list.size());
-    	for (Invoice invoice:list) {
-    		System.out.println(invoice);
-    	}
-    	return true;
-    }
-    
-    @RequestMapping("/getTabs")
-    public TabWrapper getTabs() {
-    	
-    	 List<Tab> list = new ArrayList<>();
-    	 list.add(new Tab("Tab - 1","Tab_1","This is the first tab"));
-    	 list.add(new Tab("Tab - 2","Tab_2","This is the second tab"));
-    	 list.add(new Tab("Tab - 3","Tab_3","This is the third tab"));
-    	 return new TabWrapper(true, list);
-    }
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@RequestMapping("/greeting")
+	public Greeting greeting(
+			@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template,
+				name));
+	}
+
+	@RequestMapping("/getInvoices")
+	public InvoiceJSONWrapper getInvoices() {
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+
+		List<Invoice> list = new ArrayList<>();
+		list.add(new Invoice(1000, "12345", "Victor", "123 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1001, "12346", "Mark", "456 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1002, "12347", "Carl", "789 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1003, "12348", "Zen", "784 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1004, "12349", "Chris", "258 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1005, "12346", "Benny", "322 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1006, "12351", "Sunny", "951 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1007, "12352", "Kumar", "878 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1008, "12351", "Nino", "357 Main St", df
+				.format(new Date())));
+		list.add(new Invoice(1009, "12354", "Ivan", "985 Main St", df
+				.format(new Date())));
+
+		return new InvoiceJSONWrapper(true, list);
+	}
+
+	@RequestMapping(value = "/postInvoices", method = RequestMethod.POST)
+	public boolean postInvoices(@RequestBody List<Invoice> list) {
+		System.out.println("List size: " + list.size());
+		for (Invoice invoice : list) {
+			System.out.println(invoice);
+		}
+		return true;
+	}
+
+	@RequestMapping("/getTabs")
+	public TabWrapper getTabs() {
+
+		List<Tab> list = new ArrayList<>();
+		list.add(new Tab("Tab - One", "Tab_1", "This is the first tab"));
+		list.add(new Tab("Tab - Two", "Tab_2", "This is the second tab"));
+		list.add(new Tab("Tab - Three", "Tab_3", "This is the third tab"));
+		return new TabWrapper(true, list);
+	}
+
+	@RequestMapping(value = "/processLogin", method = RequestMethod.POST)
+	public LoginWrapper processLogin(@RequestParam(value = "userId") String userId,
+			@RequestParam(value = "password") String password) {
+		System.out.println("userid: " + userId + " -- password: " + password);
+
+		return new LoginWrapper(true, "Login Success");
+	}
 
 }
--- ExtJSDemo/src/main/webapp/app.js
+++ ExtJSDemo/src/main/webapp/app.js
@@ -1,32 +1,63 @@
 Ext.onReady(function() {
+	
+	Ext.form.field.VTypes.emailText = 'Please enter a valid e-mail id';
 
-	var tabPanel = Ext.create('Ext.tab.Panel', {
-		activeTab : 0,
-		itemId : 'myTPanel'
-	});
+	var fpItems = [ {
+		fieldLabel : 'Email ID',
+		name : 'userId',
+		allowBlank : false,
+		vtype: 'email',
+		blankText: 'Email ID cannot be blank'
+	}, {
+		fieldLabel : 'Password',
+		name : 'password',
+		allowBlank : false,
+		inputType : 'password',
+		blankText: 'Password cannot be blank'
+	} ];
 
-	Ext.create('Ext.window.Window', {
-		height : 300,
+	var fp = Ext.create('Ext.form.Panel', {
+		renderTo : Ext.getBody(),
 		width : 400,
-		layout : 'fit',
-		items : tabPanel
-	}).show();
-
-	Ext.Ajax.request({
-		url : "REST/getTabs",
-		success : function(response, options) {
-			console.log('ajax success');
-			var obj = Ext.decode(response.responseText);
-			console.log(obj.data);
-			var tPanel = Ext.ComponentQuery.query('#myTPanel')[0];
-			tPanel.add(obj.data);
-			tPanel.setActiveTab('Tab_1');
+		height : 160,
+		title : 'Authenticate',
+		frame : true,
+		bodyStyle : 'padding: 6px',
+		labelWidth : 126,
+		defaultType : 'textfield',
+		defaults : {
+			msgTarget : 'side',
+			anchor : '-10'
 		},
-		failure : function(response, options) {
-			console.log('ajax failed');
-		}
+		items : fpItems,
+		buttons : [ {
+			text : 'Submit',
+		        handler: function() {
+		            // The getForm() method returns the Ext.form.Basic instance:
+		            var form = this.up('form').getForm();
+		            if (form.isValid()) {
+		                // Submit the Ajax request and handle the response
+		                form.submit({
+		                	url : 'REST/sprocessLogin',
+		                    success: function(form, action) {
+		                       Ext.Msg.alert('Success', action.result.msg);
+		                    },
+		                    failure: function(form, action) {
+		                        Ext.Msg.alert('Failed', action.result ? action.result.msg : 'No response');
+		                    }
+		                });
+		            }
+		        }
+		}, {
+			text : 'Reset',
+			handler: function() {
+			var form = this.up('form').getForm();
+			form.reset();
+			}
+		} ]
+
 	});
-
-
+	
+	fp.center();
 
 });
